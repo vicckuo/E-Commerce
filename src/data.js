948158c2e2799a615d80b9219c41452a0fe2dbd3
Vic Kuo <eun.hru@gmail.com>
2022-10-06 13:04:35 +0800

    20221006

--- src/data.js
+++ src/data.js
@@ -8,17 +8,34 @@ export const sliderItems = [
     },
     {
         id: 2,
-        img: "https://cdn.pixabay.com/photo/2017/12/26/09/15/woman-3040029_960_720.jpg",
+        img: "https://cdn.pixabay.com/photo/2017/08/05/00/12/girl-2581913_960_720.jpg",
         title: "秋季販售",
         desc: "30%折扣限量秋季衣服",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: "https://cdn.pixabay.com/photo/2017/12/26/09/15/woman-3040029_960_720.jpg",
+        img: "https://cdn.pixabay.com/photo/2020/04/08/16/05/stylish-5017914_960_720.jpg",
         title: "冬季販售",
         desc: "30%折扣限量冬季衣服",
         bg: "fbf0f4",
     },
+]
 
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2021/07/10/10/49/women-6401177_960_720.jpg",
+        title: "短袖",
+    },
+    {
+        id: 2,
+        img: "https://cdn.pixabay.com/photo/2014/08/05/10/31/waiting-410328_960_720.jpg",
+        title: "長袖",
+    },
+    {
+        id: 3,
+        img: "https://cdn.pixabay.com/photo/2015/09/05/23/26/girl-926225_960_720.jpg",
+        title: "大衣",
+    },
 ]
