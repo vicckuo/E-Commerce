e8b3c2c41eb4077c454e26907d1d068ca59aa3e7
Vic Kuo <eun.hru@gmail.com>
2022-10-11 18:03:32 +0800

    20221011

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,20 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingBasketOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive"
 
 const Container = styled.div`
     height: 60px;
+    ${mobile({ height: "50px" })}
 `
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
+    ${mobile({ padding: "10px 0px" })}
+
 `
 const Left = styled.div`
     flex: 1;
@@ -20,6 +25,7 @@ const Left = styled.div`
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
+    ${mobile({ display: "none" })}
 `
 
 const SearchContainer = styled.div`
@@ -32,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
     border: none;
+    ${mobile({ width: "50px" })}
 `
 
 const Center = styled.div`
@@ -41,6 +48,7 @@ const Center = styled.div`
 
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({ fontSize: "14px" })}
 `
 
 const Right = styled.div`
@@ -48,12 +56,14 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ flex: 2, justifyContent: "center" })}
 `
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `
 
 const Navbar = () => {
@@ -63,7 +73,7 @@ const Navbar = () => {
                 <Left>
                     <Language>中文</Language>
                     <SearchContainer>
-                        <Input />
+                        <Input placeholder="搜尋" />
                         <Search style={{ color: "gray", fontSize: 16 }} />
                     </SearchContainer>
                 </Left>
